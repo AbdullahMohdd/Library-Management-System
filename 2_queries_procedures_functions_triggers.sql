597ae74fa5a060617ff385fb23b762dfdeff0ae4
AbdullahMohdd <abd.alsaaedi@gmail.com>
2025-07-24 12:13:03 +0300

    queries, procedures, functions, and trigger script

---
+++ 2_queries_procedures_functions_triggers.sql
@@ -0,0 +1,5 @@
+-- This file contains SQL queries, stored procedures, functions, and a trigger for the Library Management System.
+
+-- Sample content:
+-- SELECT * FROM Books WHERE CurrentStatus = 'Borrowed';
+-- More complex queries, procedures, and triggers would be added here.
