d66e53961fc89681ad2545bf60a1b5d6887d7921
AbdullahMohdd <abd.alsaaedi@gmail.com>
2025-07-24 14:08:22 +0300

    Delete 2_queries_procedures_functions_triggers.sql

--- 2_queries_procedures_functions_triggers.sql
+++
@@ -1,5 +0,0 @@
--- This file contains SQL queries, stored procedures, functions, and a trigger for the Library Management System.
-
--- Sample content:
--- SELECT * FROM Books WHERE CurrentStatus = 'Borrowed';
--- More complex queries, procedures, and triggers would be added here.
